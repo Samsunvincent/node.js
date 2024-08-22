57d5d073760fe6b73592a4a633ed0f8bd7a25d2c
samsun <samsunvincen143@gmail.com>
2024-08-22 11:00:01 +0530

    taking-value

--- server/server1.js
+++ server/server1.js
@@ -4,7 +4,8 @@ const http = require('http');
 const PORT = 3000;
 const url = require('url')
 const fs = require('fs');
-const { parse } = require('path');
+// const { parse } = require('path');
+const querystring = require('querystring')
 
 const server = http.createServer((req,res) =>{
   
@@ -31,7 +32,26 @@ const server = http.createServer((req,res) =>{
     res.writeHead(200,{'Content-Type' : "text/css"});
     res.end(fs.readFileSync('../client/style.css'));
   }
-  elseif
+  else if(parsed_url.pathname === '/submit' && req.method === 'POST'){
+   
+   console.log('reached here')
+    let body = '';
+    req.on('data',(chunks) =>{
+      console.log(chunks);
+      body = body+chunks.toString();
+    });
+    req.on('end',()=>{
+      console.log('body',body)
+      const datas = querystring.parse(body);
+      console.log('datas',datas);
+
+      console.log('name',datas.name);
+      console.log('email',datas.email);
+    })
+
+
+  }
+ 
 
   
 });
